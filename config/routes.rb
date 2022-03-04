e5da19f5d25a0bd20ee50e0707eef2b16eb55319
samcortez07 <samuelscortez@outlook.com>
2022-03-04 18:47:35 +0000

    Try

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
 
+  get("/square/new", {:controller => "", :actiion => "blank_square_form"})
+
+  devise_for :admin_users, ActiveAdmin::
+  ActiveAdmin.routes(self)
+
 end
